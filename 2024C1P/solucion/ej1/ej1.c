0445e73ed47a4cbd82b83fec335afd38a7ba8a01
jul1martin <julianmartiniscoff@gmail.com>
2025-05-05 03:11:15 +0000

    avance en c de parcial 2024 1c

--- 2024C1P/solucion/ej1/ej1.c
+++ 2024C1P/solucion/ej1/ej1.c
@@ -16,13 +16,36 @@ ordering_table_t* inicializar_OT(uint8_t table_size) {
   ordering_table_t* ot = malloc(sizeof(ordering_table_t));
 
   ot->table_size = table_size;
-  ot->table = malloc(sizeof(nodo_ot_t) * table_size);
+  ot->table = malloc(sizeof(nodo_ot_t*) * table_size);
 
+  for (size_t i = 0; i < table_size; i++) {
+    ot->table[i] = NULL;
+  }
+  
   return ot;
 }
 
 void calcular_z(nodo_display_list_t* nodo, uint8_t z_size) {
+  nodo->z = ((nodo->primitiva)(nodo->x, nodo->y, z_size));
 }
 
 void ordenar_display_list(ordering_table_t* ot, nodo_display_list_t* display_list) {
+  calcular_z(display_list, ot->table_size);
+
+  nodo_ot_t* node = ot->table[display_list->z];
+
+  while(node != NULL) {
+    nodo_ot_t* oldNode = node;
+
+    node = node->siguiente;
+
+    if(node == NULL) {
+      oldNode->siguiente = node;
+    }
+  }
+
+  node->display_element = display_list;
+  node->siguiente = NULL;
+
+  return;
 }
